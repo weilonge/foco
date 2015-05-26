840bc03d63e4a235a490593003eeb7697ff30b85
Sean Lee <weilonge@gmail.com>
2015-05-26 10:41:29 +0800

    Add priority changing feature.

--- examples/basic.js
+++ examples/basic.js
@@ -1,13 +1,24 @@
 var foco = require('../lib/foco');
 
-var pq = foco.priorityQueue(function (id, data, done){
+var test = {};
+test.greetings = 'Hello!';
+test.handler = function (id, data, done){
+  var that = this;
   console.log('task ' + id + ' processing...');
   setTimeout(function (){
     console.log('task ' + id + ' : ' + data);
+    console.log(that.greetings + ', ' + data);
     done();
   }, 1500);
-}, 4);
+};
+
+var pq = foco.priorityQueue(test.handler.bind(test), 4);
 
 for (var i = 1; i < 20; i++) {
-  pq.push(i, i*100, i*1000);
+  pq.push(i, i, i*1000);
 }
+
+pq.priorityChange(19, 9);
+pq.priorityChange(18, 8);
+pq.priorityChange(17, 7);
+pq.priorityChange(16, 6);
